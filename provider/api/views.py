32f56d486c5d2114f455457cf9392a4e48ce872a
Lucas <lucasmiguelmac@gmail.com>
2022-06-09 02:57:33 +0000

    Add cache to viewsets

--- provider/api/views.py
+++ provider/api/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 
 from provider.api.serializers import ProviderSerializer, ServiceAreaSerializer
@@ -8,6 +10,14 @@ class ProviderModelViewset(ModelViewSet):
     serializer_class = ProviderSerializer
     queryset = Provider.objects.all()
 
+    @method_decorator(cache_page(60*60*2))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 class ServiceAreaModelViewset(ModelViewSet):
     serializer_class = ServiceAreaSerializer
     queryset = ServiceArea.objects.all().select_related("provider")
+
+    @method_decorator(cache_page(60*60*2))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
