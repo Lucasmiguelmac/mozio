0e9526a7f50663556a45ada623c67b821faf931c
Lucas <lucasmiguelmac@gmail.com>
2022-06-09 02:00:21 +0000

    Test servicearea endpoints

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -2,10 +2,22 @@ import pytest
 from django.urls import resolve, reverse
 
 
-class TestEndpointsURLs:
+class TestProviderURLs:
     @pytest.mark.parametrize('url_name, expected_path, kwargs, viewname',[
         ('provider-list',   '/api/providers/',      None,       'ProviderModelViewset'),
         ('provider-detail', '/api/providers/1/',    {'pk': 1},  'ProviderModelViewset'),
+    ])
+    def test_url_name(self, url_name, expected_path, kwargs, viewname):
+        url = reverse(url_name, kwargs=kwargs)
+        assert url == expected_path
+
+        resolver = resolve(expected_path)
+        assert resolver.func.__name__ == viewname
+
+class TestServiceAreaURLs:
+    @pytest.mark.parametrize('url_name, expected_path, kwargs, viewname',[
+        ('servicearea-list',   '/api/service_areas/',      None,       'ServiceAreaModelViewset'),
+        ('servicearea-detail', '/api/service_areas/1/',    {'pk': 1},  'ServiceAreaModelViewset'),
     ])
     def test_url_name(self, url_name, expected_path, kwargs, viewname):
         url = reverse(url_name, kwargs=kwargs)
