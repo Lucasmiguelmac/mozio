c467da7a41b72eeaeed209082e2c87cde3dbe0ec
Lucas <lucasmiguelmac@gmail.com>
2022-06-10 05:10:20 +0000

    Setup documentation

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('api/', include('provider.api.urls')),
+    path('docs/', include_docs_urls(title='Providers & Service Areas API')),
 ]
